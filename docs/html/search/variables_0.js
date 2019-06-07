78e8ab5d1cbd4ab3503203ba1d9e4a614188c9e7
Tristan Krause <tristan.krause@stud.htwk-leipzig.de>
2019-06-07 10:06:21 +0200

    save

--- docs/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['baudrate',['BAUDRATE',['../classB15F.html#a7d548d6861cfc69753161bf9cda14f87',1,'B15F']]]
-];
--- docs/html/search/variables_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pre',['PRE',['../classB15F.html#a3b0fc1f85954b2d9c145af4a3af5b1ec',1,'B15F']]]
-];
