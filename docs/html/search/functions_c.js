ad457a5f72db561915c59dc32cdeda0bd6c1ca0f
devfix <tristan.krause@stud.htwk-leipzig.de>
2019-06-11 14:58:11 +0200

    cleanup doc

--- docs/html/search/functions_c.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['what',['what',['../classTimeoutException.html#a97eaf01fc39ddb94b060020b42fefd6e',1,'TimeoutException::what()'],['../classUSARTException.html#a2af5e3c00cd0585c7427c2e0420a8f15',1,'USARTException::what()']]],
-  ['writetofile',['writeToFile',['../classPlottyFile.html#a82c348e7fade2edcbc907e7c2bc2e305',1,'PlottyFile']]]
-];
